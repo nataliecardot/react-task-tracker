d188e15b1595aab89b11c6bd0344ef61e99d6fc7
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-27 15:49:07 -0800

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+// type impt to create this with ES7 React Snippets extension
+import PropTypes from 'prop-types';
+
+// Instead of (and using {props.title}): const Header = (props) => {
+const Header = ({ title }) => {
+  return (
+    <header>
+      {/* <h1 style={{ color: 'red', backgroundColor: '#000' }}>{title}</h1> */}
+      {/* <h1 style={headingStyle}>{title}</h1> */}
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+// const HeadingStyle = {
+//   color: 'red',
+//   backgroundColor: '#000',
+// };
+
+export default Header;
