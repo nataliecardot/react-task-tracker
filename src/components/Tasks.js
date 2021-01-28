4e59f9feda3c91ee26f1f58d28c33b96fd88f82f
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-27 18:39:27 -0800

    Add Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,3 +1,5 @@
+import Task from './Task';
+
 const Tasks = ({ tasks }) => {
   return (
     // To add to tasks, would do: (with new data in object)
@@ -5,7 +7,7 @@ const Tasks = ({ tasks }) => {
     // You couldn't do tasks.push() because state is immutable; recreate it and send it down - don't directly change; it's one-way data
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </>
   );
