3903b3495c9ce8c7c5f058393e7778ee397a5a93
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-28 21:21:26 -0800

    Enable task deletion

--- src/App.js
+++ src/App.js
@@ -22,10 +22,19 @@ function App() {
     },
   ]);
 
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No tasks to display.'
+      )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,13 @@
 import Task from './Task';
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
   return (
     // To add to tasks, would do: (with new data in object)
     // setTasks([...tasks, {}])
     // You couldn't do tasks.push() because state is immutable; recreate it and send it down - don't directly change; it's one-way data
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} />
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   );
