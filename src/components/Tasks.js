22f4f842930081c03416058e6d55680b4c2d0d0e
Natalie-Cardot <nataliecardot@gmail.com>
2021-02-02 07:32:23 -0800

    Enable toggle reminder

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,18 @@
 import Task from './Task';
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     // To add to tasks, would do: (with new data in object)
     // setTasks([...tasks, {}])
     // You couldn't do tasks.push() because state is immutable; recreate it and send it down - don't directly change; it's one-way data
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete} />
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
