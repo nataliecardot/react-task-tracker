60966b0aaa10f0d02ecb0986db6535b9f37697de
Natalie-Cardot <nataliecardot@gmail.com>
2021-02-02 19:40:51 -0800

    Add onClick

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 
 const Button = ({ color, text, onClick }) => {
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
       {text}
     </button>
   );
